cb1fe8f8bbcba194c9325bb5526f69c7d06c7e12
kuwabara <kw7101410@gmail.com>
2014-05-30 14:24:29 +0900

    最初のファイル

---
+++ mouse/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //2次元クラスPoint型の変数cposを宣言
+            Point cpos;
+
+            //cposにマウスのフォーム座標を取り出す
+            cpos = this.PointToClient(MousePosition);
+
+            //フォームにマウス座標を表示
+            Text = "" + cpos.X + "," + cpos.Y;
+
+            //マウス座標にラベルをくっつける。(マウスの座標をlabelに代入)
+            label1.Left = cpos.X;
+            label1.Top = cpos.Y;
+        }
+    }
+}
