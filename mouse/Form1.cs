48a00c775db6b3ca6056c294c8c6503064091473
Yu Tanaka <tanaka@am1.jp>
2014-05-30 13:55:50 +0900

    csとdesignerを追加

---
+++ mouse/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // 2次元クラスPoint型の変数cposを宣言
+            Point cpos;
+
+            // cposに、マウスのフォーム座標を取り出す
+            cpos = this.PointToClient(MousePosition);
+
+            // ラベル(フォーム)にマウス座標を表示
+            Text = "" + cpos.X + "," + cpos.Y;
+        }
+    }
+}
