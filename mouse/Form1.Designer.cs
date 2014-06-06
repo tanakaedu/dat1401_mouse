8c810af2fbb552945ad52182f9751a6d37fb326e
sanpei <levphantoma@gmail.com>
2014-06-06 14:50:20 +0900

    ラベルに移動した際に反転する機能 ラベルにマウスカーソルを合わせるとラベルが止まる機能の実装

--- mouse/Form1.Designer.cs
+++ mouse/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace mouse
+﻿namespace Mouse
 {
     partial class Form1
     {
@@ -29,11 +29,75 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(・ω・)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(310, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "＼(^o^)／";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 528);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- mouse/Form1.cs
+++ mouse/Form1.cs
@@ -7,13 +7,72 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace mouse
+namespace Mouse
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                // textBoxからintの変数に値を取得
+                int vx = int.Parse(textBox1.Text);
+                int vy = int.Parse(textBox2.Text);
+
+                // 2次元クラスPoint型の変数cposを宣言
+                Point cpos;
+
+                // cposに、マウスのフォーム座標を取り出す
+                cpos = this.PointToClient(MousePosition);
+
+                // ラベル(フォーム)にマウス座標を表示
+                Text = "" + cpos.X + "," + cpos.Y;
+
+                // マウス座標にラベルをくっつけてみよう
+                label1.Left = cpos.X;
+                label1.Top = cpos.Y;
+
+                // ラベル2の移動
+                label2.Left = label2.Left + vx;
+                label2.Top = label2.Top + vy;
+
+                // ラベルが左右オーバーした時
+                if ((label2.Left < 0) || (label2.Left + label2.Width > ClientSize.Width))
+                {
+                    // 左右反転させる
+                    label2.Left -= vx;
+                    textBox1.Text = (-vx).ToString();
+                }
+                // ラベルが上下オーバーした時
+                if ((label2.Top < 0) || (label2.Top + label2.Height > ClientSize.Height))
+                {
+                    // 上下反転させる
+                    label2.Top -= vy;
+                    textBox2.Text = (-vy).ToString();
+                }
+
+                // ラベルに当たり判定をつける
+                if ((label2.Left < cpos.X) && (label2.Left + label2.Width > cpos.X))
+                {
+                    textBox1.Text = "0";
+                    textBox2.Text = "0";
+                }
+                if ((label2.Top < cpos.Y) && (label2.Top + label2.Height > cpos.Y))
+                {
+                    textBox1.Text = "0";
+                    textBox2.Text = "0";
+                }
+
+            }
+            catch (Exception ee)
+            {
+            }
+
         }
     }
 }
