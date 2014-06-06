63919dca1b2219cd8a4043df53d19eee64fcbcad
okura <loinoabo@nyus.com>
2014-06-06 14:50:03 +0900

    ラベルの移動とマウスで止める

--- mausu/mausu/Form1.cs
+++ mausu/mausu/Form1.cs
@@ -18,17 +18,64 @@ namespace mausu
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            Point cpos;
+            try
+            {
+                int vx = int.Parse(textBox1.Text);
+                int vy = int.Parse(textBox2.Text);
+                label2.Left = label2.Left + vx;
+                label2.Top = label2.Top + vy;
+                
+                
+                if (label2.Left < 0)
+                {
+                    textBox1.Text = "15";
+                }
+                if (label2.Top < 0)
+                {
+                    textBox2.Text = "15";
+                }
+                if (label2.Left + label2.Width > ClientSize.Width)
+                {
+                    label2.Left -= vx;
+                    textBox1.Text = (-vx).ToString();
+                }
+                if (label2.Top + label2.Height > ClientSize.Height)
+                {
+                    label2.Top -= vy;
+                    textBox2.Text = (-vy).ToString();
+                }
+                
+                Point cpos;
 
-            cpos = this.PointToClient (MousePosition);
+                cpos = this.PointToClient(MousePosition);
 
-            Text = "" + cpos.X + "," + cpos.Y;
-            label1.Left=cpos.X;
-            label1.Top=cpos.Y;
-        }
+                Text = "" + cpos.X + "," + cpos.Y;
+                label1.Left = cpos.X;
+                label1.Top = cpos.Y;
+            }
+
+            catch (Exception ee)
+            { 
+            }
+           }
 
         private void label1_Click(object sender, EventArgs e)
         {   
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
