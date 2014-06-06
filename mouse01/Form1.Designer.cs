57f4de36efc275e5dbca26c49607ba6f3e447a4f
fukumoto <kamikaze96neko@yahoo.co.jp>
2014-06-06 14:50:30 +0900

    ラベルの移動とマウスで止める

--- mouse01/Form1.Designer.cs
+++ mouse01/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // timer1
@@ -44,16 +47,45 @@
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label1.Location = new System.Drawing.Point(138, 149);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 24);
+            this.label1.Size = new System.Drawing.Size(179, 24);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "（・ω・`)ノ　マテ～";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(70, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "（/ω//)＝３いやぁ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 19);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "90";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 19);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "60";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(966, 416);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -66,6 +98,9 @@
 
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- mouse01/Form1.cs
+++ mouse01/Form1.cs
@@ -29,8 +29,50 @@ namespace mouse01
 
             label1.Left = cpos.X;
             label1.Top = cpos.Y;
-        }
 
+            try
+            {
+
+                //textBoxからintの変数の値を取得
+                int vx = int.Parse(textBox1.Text);
+                int vy = int.Parse(textBox2.Text);
+
+                //ラベルの座標に加算
+                label2.Left = label2.Left + vx;
+                label2.Top = label2.Top + vy;
+
+                //　左にオーバーした時
+                //　「あるいは」
+                // 　右にオーバーした時
+                if ((label2.Left <= 0) || (label2.Right >= ClientSize.Width))
+                {
+                    //　左右反転させる
+                    label2.Left -= vx;
+                    textBox1.Text = (-vx).ToString();
+                }
+
+                if ((label2.Top <= 0) || (label2.Bottom >= ClientSize.Height))
+                {
+                    label2.Top -= vy;
+                    textBox2.Text = (-vy).ToString();
+                }
+            }
+            catch (Exception ee)
+            {
+            }
+                //ぶつかったら止まる
+                if ((label2.Top < cpos.Y) && (label2.Top+label2.Height >= cpos.Y))
+                {
+                    if ((label2.Left < cpos.X) && (label2.Left + label2.Width >= cpos.X))
+                    {
+                        textBox1.Text = "0";
+                        textBox2.Text = "0";
+                        label2.Text = "クリア！！";
+                        label1.Text = "つっかまえた！！";
+                    }
+                }
+        }
+       
         private void label1_Click(object sender, EventArgs e)
         {
 
