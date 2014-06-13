8f1f43357059908433a47b90310d42c7ecaf75e2
syoma <k.0824.xoxo_@docomo.ne.jp>
2014-06-13 14:02:56 +0900

    textBoxからiVelへ

--- mouse/Form1.cs
+++ mouse/Form1.cs
@@ -12,8 +12,8 @@ namespace mouse
     public partial class Form1 : Form
     {
 
-        int iValX = 10;
-        int iValY = 10;
+        int iVelX = 10;
+        int iVelY = 10;
 
         public Form1()
         {
@@ -35,8 +35,8 @@ namespace mouse
             //ラベル2の移動
             try
             {
-                int vx = int.Parse(textBox1.Text);
-                int vy = int.Parse(textBox2.Text);
+                int vx = iVelX;
+                int vy = iVelY;
 
                 // ラベルの座標に加算
                 label2.Left = label2.Left + vx;
@@ -44,29 +44,20 @@ namespace mouse
                 if ((label2.Left < 0) || (label2.Left + label2.Width > ClientSize.Width))
                 {
                     label2.Left -= vx;
-                    textBox1.Text = (-vx).ToString();
+                    iVelX = -vx;
                 }
                 if ((label2.Top < 0) || (label2.Top + label2.Height > ClientSize.Height))
                 {
                     label2.Top -= vy;
-                    textBox2.Text = (-vy).ToString();
+                    iVelY = -vy;
                 }
                 if ((label2.Left < cpos.X) && (label2.Left + label2.Width > cpos.X)&&(label2.Top < cpos.Y) && (label2.Top + label2.Height > cpos.Y))
                 {
-                    //a+=5;
-                    textBox1.Text = "0";//a.ToString();
-                    textBox2.Text = "0";//a.ToString();
+                    iVelX = 0;
+                    iVelY = 0;
                     label2.Text = "j(´-｀)しﾀｶｼ";
                 }
-                /*if (a > 1000)
-                {
-                    a = 0;
-                }
-               if ((label2.Left<label3.Left)&&(label2.Left +label2.Width > label3.Left+label3.Width)&&(label2.Top<label3.Top)&&(label2.Top+label2.Height >label3.Top+label3.Height))
-                {
-                    textBox1.Text = (-vx).ToString();
-                    textBox2.Text = (-vy).ToString();
-                }*/               
+               
             }
             catch (Exception ee)
             {
@@ -77,10 +68,10 @@ namespace mouse
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text ="50";
-            textBox2.Text ="50";
+            iVelX = 10;
+            iVelY = 10;
             label2.Text = "j(´-｀)しﾂｶﾚﾀﾜ";
-            a = 0;
+            //a = 0;
         }
     }
 }
