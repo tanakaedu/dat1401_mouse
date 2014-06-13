1e4a3b57b02448dac195a186942e465999ed528d
sanpei <levphantoma@gmail.com>
2014-06-13 14:52:30 +0900

    初速を乱数に変更

--- mouse/Form1.cs
+++ mouse/Form1.cs
@@ -11,8 +11,9 @@ namespace Mouse
 {
     public partial class Form1 : Form
     {
-        int iVelX = 10;
-        int iVelY = 10;
+        int iVelX = rand.Next(-10, 11);
+        int iVelY = rand.Next(-10, 11);
+        private static Random rand = new Random();
 
         public Form1()
         {
