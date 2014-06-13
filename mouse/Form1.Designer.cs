f91eeec3116e8f044e9cbf178f091763458c07fb
husimi <o>
2014-06-13 13:37:43 +0900

    速度をインスタンス変数変更する前

--- mouse/Form1.Designer.cs
+++ mouse/Form1.Designer.cs
@@ -54,9 +54,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label2.Location = new System.Drawing.Point(173, 176);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.Size = new System.Drawing.Size(60, 21);
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -67,7 +68,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 19);
             this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "30";
+            this.textBox1.Text = "5";
             // 
             // textBox2
             // 
@@ -75,7 +76,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 19);
             this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "-50\r\n";
+            this.textBox2.Text = "8\r\n";
             // 
             // Form1
             // 
--- mouse/Form1.cs
+++ mouse/Form1.cs
@@ -11,6 +11,9 @@ namespace mouse
 {
     public partial class Form1 : Form
     {
+        int iValX = 10;
+        int iValY = 10;
+
         public Form1()
         {
             InitializeComponent();
@@ -18,8 +21,6 @@ namespace mouse
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
-
             try
             {
                 // 2次元クラスPoint型の変数cposを宣言
@@ -40,46 +41,61 @@ namespace mouse
                 label2.Left = label2.Left + vx;
                 label2.Top = label2.Top + vy;
 
-                if ((label2.Left < 0) || (label2.Right > ClientSize.Width))
+                //反射
+                if ((label2.Left < 0) || (label2.Left+label2.Width >  ClientSize.Width))
                 {
+                   //左右反転
                     label2.Left -= vx;
                     textBox1.Text = (-vx).ToString();
                 }
-                if ((label2.Top < 0) || (label2.Bottom > ClientSize.Height))
+                if ((label2.Top < 0) || (label2.Top + label2.Height >  ClientSize.Height))
                 {
+                    //上下反転
                     label2.Top -= vy;
                     textBox2.Text = (-vx).ToString();
                 }
-                if ((label2.Left >= cpos.X) && (label1.Right + cpos.X > ClientSize.Width))
+
+                //cpos.x:マウスのX座標
+                //cpos.y:マウスのY座標
+                //if((条件１）＆＆（条件２）＆＆（条件３）＆＆（じょうけん４）
+                //→条件1から４まで全部成立したときのif文
+                //条件1cpos.xは.label2.left以上
+                //条件２cpos.xは.label2.left+label2.Wideth未満
+                //条件３cpos.ｙは.label2.Top以上
+                //条件４cpos.ｙは.label2.Top+label2.Height未満
+                if (( cpos.X >= label2.Left ) 
+                    && (cpos.X < label2.Left+label2.Width)
+                    && (cpos.Y >= label2.Top  ) 
+                    && (cpos.Y < label2.Top+label2.Height))
                 {
-                    label2.Left = label1;
+                    textBox1.Text = "0";
+                    textBox2.Text = "0";
                 }
-                if ((label2.Top >= cpos.Y) && (label1.Bottom + cpos.Y > ClientSize.Height))
-                {
-                    label2.Top = label1;
-                }
-                /*if (label1.Right > ClientSize.Width)
-                {
-                    textBox1.Text = "-10";
-                    textBox2.Text = "1";
-                }*/
-                /*if (label1.Bottom >ClientSize.Height)
-                {
-                    textBox1.Text = "1";
-                    textBox2.Text = "-10";
-                }*/
+
             }
+            /*
+             
+             if (label1.Right > ClientSize.Width)
+            {
+                textBox1.Text = "-10";
+                textBox2.Text = "1";
+            }
+            if (label1.Bottom >ClientSize.Height)
+            {
+                textBox1.Text = "1";
+                textBox2.Text = "-10";
+            }*/
+            
             catch (Exception ee)
             {
-   
-            }
-            
-        }
 
+            }
+        }
         private void label2_Click(object sender, EventArgs e)
         {
             label2.Text = "-10";
             label2.Text = "0";
         }
+
     }
 }
