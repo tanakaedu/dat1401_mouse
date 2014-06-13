b3352a0be8a5cbd895fafe9b06903d5e7ac20bf0
syoma <k.0824.xoxo_@docomo.ne.jp>
2014-06-13 16:03:04 +0900

    ラベルの量産と速度の配列の作成 ＞ラベルの動作は未実装

--- mouse/Form1.Designer.cs
+++ mouse/Form1.Designer.cs
@@ -31,10 +31,10 @@
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // timer1
@@ -46,59 +46,63 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(360, 157);
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(275, 386);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(209, 29);
+            this.label1.Size = new System.Drawing.Size(116, 48);
             this.label1.TabIndex = 0;
-            this.label1.Text = "(\'A`;)ｶ、ｶｰﾁｬﾝ...";
+            this.label1.Text = "――";
             this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(44, 19);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "5";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(62, 12);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(44, 19);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "5";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label2.Location = new System.Drawing.Point(242, 120);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(131, 37);
+            this.label2.Size = new System.Drawing.Size(33, 35);
             this.label2.TabIndex = 3;
-            this.label2.Text = "j(´-｀)し";
+            this.label2.Text = "・";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(112, 12);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(52, 19);
+            this.button1.Size = new System.Drawing.Size(70, 32);
             this.button1.TabIndex = 4;
             this.button1.Text = "STOP";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "配列練習";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 32);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "for練習";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(709, 477);
+            this.ClientSize = new System.Drawing.Size(1177, 435);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -111,10 +115,10 @@
 
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- mouse/Form1.cs
+++ mouse/Form1.cs
@@ -11,14 +11,36 @@ namespace mouse
 {
     public partial class Form1 : Form
     {
+        Label[] chrs = new Label[10];
+        int[] iVX = new int[15];
+        int[] iVY = new int[15];
+        
 
-        int iVelX = rand.Next(20);
-        int iVelY = rand.Next(20);
+        
+        int iVelX = rand.Next(10);
+        int iVelY = rand.Next(10);
         private static Random rand = new Random();
 
+        //コンストラクタ
+        //Form1クラスが生成される時に実行する
+        //特別な関数
         public Form1()
         {
             InitializeComponent();
+            
+            //ラベルの生成
+            for (int i = 0; i < 10; i++)
+            {
+                chrs[i] = new Label();
+                chrs[i].AutoSize = true; //ミソ
+                chrs[i].Text = "・";
+                chrs[i].Left = rand.Next(ClientSize.Width);
+                chrs[i].Top = rand.Next(ClientSize.Height);
+                Controls.Add(chrs[i]); //フォームに追加
+
+                iVX[i] = rand.Next(-20, 0);
+                iVY[i] = rand.Next(-20, 0);
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -31,7 +53,7 @@ namespace mouse
             // フォームにマウス座標を表示
             Text = "" + cpos.X + "," + cpos.Y;
             label1.Left = cpos.X;
-            label1.Top = cpos.Y;
+            //label1.Top = cpos.Y;
 
             //ラベル2の移動
             try
@@ -52,11 +74,10 @@ namespace mouse
                     label2.Top -= vy;
                     iVelY = -vy;
                 }
-                if ((label2.Left < cpos.X) && (label2.Left + label2.Width > cpos.X)&&(label2.Top < cpos.Y) && (label2.Top + label2.Height > cpos.Y))
+                if((label2.Left < cpos.X) && (label2.Left + label2.Width > cpos.X) && (label2.Top < cpos.Y) && (label2.Top + label2.Height > cpos.Y))
                 {
-                    iVelX = rand.Next(-99,100);
-                    iVelY = rand.Next(-99,100);
-                    label2.Text = "j(´-｀)しﾀｶｼ";
+                    iVelX = rand.Next(-10,10);
+                    iVelY = rand.Next(-10,0);
                 }
                
             }
@@ -71,7 +92,41 @@ namespace mouse
         {
             iVelX = rand.Next(10);
             iVelY = rand.Next(10);
-            label2.Text = "j(´-｀)しﾂｶﾚﾀﾜ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //int型の配列変数3つを定義
+            int[] iar = new int[3];
+            //[]の中に添え字を入れることで、
+            //別の場所にアクセスできる
+            iar[0] = 0;
+            iar[1] = 1;
+            iar[2] = 2;
+            MessageBox.Show(iar[0].ToString());
+            MessageBox.Show(iar[1].ToString());
+            MessageBox.Show(iar[2].ToString());
+            int i = 0;
+            MessageBox.Show(iar[i].ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                if (i < 3)
+                {
+                    continue;
+                }
+
+                MessageBox.Show(i.ToString());
+                if (i >= 6)
+                {
+                    break;
+                }
+            }
+            MessageBox.Show("iは" + i);
         }
     }
 }
