a5022fcbd3990dcf9fbcc8c798dc31d96e4eb475
kousuke <nikoemon@ezweb.ne.jp>
2014-06-13 13:30:49 +0900

    速度をインスタンス変数変更する前

--- mouse/Form1.Designer.cs
+++ mouse/Form1.Designer.cs
@@ -35,6 +35,10 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // timer1
@@ -46,12 +50,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label1.Location = new System.Drawing.Point(130, 114);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 12);
+            this.label1.Size = new System.Drawing.Size(158, 19);
             this.label1.TabIndex = 0;
             this.label1.Text = "まって～(／・ω・)／";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
@@ -67,13 +71,13 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 19);
             this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "10";
+            this.textBox2.Text = "0";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label3.Location = new System.Drawing.Point(103, 213);
+            this.label3.Location = new System.Drawing.Point(86, 232);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(388, 64);
             this.label3.TabIndex = 4;
@@ -87,13 +91,51 @@
             this.button1.TabIndex = 5;
             this.button1.Text = "止まる";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(627, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(371, 64);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "＼(｀・ω・´)ゝ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(70, 434);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(142, 19);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "-10";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(333, 439);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(157, 19);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(592, 421);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 70);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "止まる";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1100, 549);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
@@ -101,7 +143,6 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,6 +156,10 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- mouse/Form1.cs
+++ mouse/Form1.cs
@@ -11,7 +11,9 @@ namespace mouse
 {
     public partial class Form1 : Form
     {
-        int a = 0;
+        //int a = 0;
+        int ivalX = 10;
+        int iValY = 10;
         
         public Form1()
         {
@@ -35,14 +37,19 @@ namespace mouse
             //ラベル2の移動
             try
             {
-                
+
                 int vx = int.Parse(textBox1.Text);
                 int vy = int.Parse(textBox2.Text);
 
-                
+                int vx2 = int.Parse(textBox3.Text);
+                int vy2 = int.Parse(textBox4.Text);
+
+
                 label3.Left = label3.Left + vx;
                 label3.Top = label3.Top + vy;
-            //ラベルの反射
+                label2.Left = label2.Left + vx2;
+                label2.Top = label2.Top + vy2;
+                //ラベルの反射:ラベル1
                 if ((label3.Left < 0) || (label3.Left + label3.Width > ClientSize.Width))
                 {
                     label3.Left -= vx;
@@ -55,22 +62,62 @@ namespace mouse
                     textBox2.Text = (-vy).ToString();
                 }
 
-                if ((label3.Left < cpos.X) && (label3.Left + label3.Width > cpos.X)&&(label3.Top < cpos.Y) && (label3.Top + label3.Height > cpos.Y))
+                if ((label3.Left < cpos.X) && (label3.Left + label3.Width > cpos.X) && (label3.Top < cpos.Y) && (label3.Top + label3.Height > cpos.Y))
                 {
-                    a+=10;
+                   /* a += 10;
                     textBox1.Text = a.ToString();
                     textBox2.Text = a.ToString();
+                    */
+                    textBox1.Text = "0";
+                    textBox2.Text = "0";
+
+                //ラベルの反射：ラベル2
+                }
+                if ((label2.Left < 0) || (label2.Left + label2.Width > ClientSize.Width))
+                {
+                    label2.Left -= vx2;
+                    textBox3.Text = (-vx2).ToString();
+                }
+
+                if ((label2.Top < 0) || (label2.Top + label2.Height > ClientSize.Height))
+                {
+                    label2.Top -= vy2;
+                    textBox4.Text = (-vy2).ToString();
+                }
+
+                if ((label2.Left < cpos.X) && (label2.Left + label2.Width > cpos.X) && (label2.Top < cpos.Y) && (label2.Top + label2.Height > cpos.Y))
+                {
+                    /* a += 10;
+                    textBox1.Text = a.ToString();
+                    textBox2.Text = a.ToString();
+                    */
+                    textBox3.Text = "0";
+                    textBox4.Text = "0";
 
                 }
 
-
+                //反射試し
+                if ((label2.Left == label3.Left + label3.Width) || (label2.Left + label2.Width == label3.Left))
+                {
+                    label3.Left -= vx;
+                    textBox1.Text = (-vx).ToString();
+                    label2.Left -= vx2;
+                    textBox3.Text = (-vx2).ToString();
+                }
+                /*if ((label2.Top < label3.Top + label3.Height) || (label3.Top > label2.Top + label2.Height))
+                {
+                    label3.Top -= vy;
+                    textBox2.Text = (-vy).ToString();
+                    label2.Top -= vy2;
+                    textBox4.Text = (-vy2).ToString();
+                }*/
             }
-
 
             catch (Exception ee)
             {
             }
-            }
+        }
+            
 
 
         private void label1_Click(object sender, EventArgs e)
@@ -87,6 +134,13 @@ namespace mouse
         {
             textBox1.Text = "0";
             textBox2.Text = "0";
+            a = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "0";
+            textBox4.Text = "0";
             a = 0;
         }
 
